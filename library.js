9ea5bad46610696b46c3a922fd5cc3aef4223e04
Svar Ajwani <ajwanisvar@gmail.com>
2024-07-28 11:29:35 -0700

    Unfinished.
    Added displayBooks function.

--- library.js
+++ library.js
@@ -15,14 +15,51 @@ function displayBooks(){
     myLibrary.forEach((book) =>{
         const card = document.createElement("div");
         card.classList.add('card');
+        container.appendChild(card);
+
         const title = document.createElement("div");
         title.classList.add('title');
         title.textContent = book.title;
         card.appendChild(title);
-        container.appendChild(card);
+
+        authorHeading = document.createElement("div");
+        authorHeading.classList.add("info-heading");
+        authorHeading.textContent = "Author:";
+        card.appendChild(authorHeading);
+
+        author = document.createElement("div");
+        author.classList.add("info");
+        author.textContent = book.author;
+        authorHeading.appendChild(author);
+
+        pagesHeading = document.createElement("div");
+        pagesHeading.classList.add("info-heading");
+        pagesHeading.textContent = "Pages:";
+        card.appendChild(pagesHeading);
+        
+        pages = document.createElement("div");
+        pages.classList.add("info")
+        pages.textContent = book.pages;
+        pagesHeading.appendChild(pages);
+        
+        statusHeading = document.createElement("div");
+        statusHeading.classList.add("info-heading");
+        statusHeading.textContent = "Status:"
+        card.appendChild(statusHeading);
+
+        read = document.createElement("div");
+        read.classList.add("info");
+        if(book.read === "true"){
+            read.textContent = "Read";
+        }
+        else {
+            read.textContent = "Not read yet";
+        }
+        statusHeading.appendChild(read);
+
     })
 }
 
-const bookExample = new Book("Techniques", "CringeCamTV", 35, true);
+const bookExample = new Book("Techniques", "CringeCamTV", 35, "true");
 myLibrary.push(bookExample);
 displayBooks();
